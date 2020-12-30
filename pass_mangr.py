3f98ef466f0c333274d71ec33da74ee11ab2fe12
SoulNinja-dev <aadhi123awesome@gmail.com>
2020-12-30 08:32:06 +0530

    login works at lasttt

--- pass_mangr.py
+++ pass_mangr.py
@@ -1,47 +1,40 @@
 import mysql.connector
-import os 
+import os
 db = mysql.connector.connect(
-        host="localhost",
-        user="soulninja",
-        password="password17",
-        database="password_manager"
-        )
+    host="localhost",
+    user="soulninja",
+    password="password17",
+    database="password_manager"
+)
 
-mycursor = db.cursor()
+cursor = db.cursor()
 
+name = input("Enter your name: ")
+print("Got it "+name)
+
+def login():
+    
+    qry="SELECT name FROM main WHERE name=%s"
+    cursor.execute(qry, (name,))
+
+    isUser=cursor.fetchone()
+
+    if isUser == None:
+        print("You might wanna sign up")
+    else:
+        print("You're in boss")
+
+login()
 # CREATE TABLE main(
 #    -> id INT AUTO_INCREMENT,
 #    -> name VARCHAR(100),
 #    -> website VARCHAR(255),
 #    -> password VARCHAR(255),
 #    -> PRIMARY KEY(id)
-#    -> );
-
-name = input("Hey welcome, Whats your good name? ") 
-print("Sounds good ",name)
-website = input("Now which website do you want to save your password in? ")
-print("Gotcha")
-password = input("now the password.. ")
-
-mycursor.execute("INSERT main (name,website,password) VALUES (%s,%s,%s)", (name, website, password))
-db.commit()
-
-
-
-def welcome():
-    name = input("Hey wassup, what's your good name?")
-    try:
-    
-
-
-os.system("clear")
-print("clearing screen.... ")
-print("Saved all right homie, you can leave now!")
-print("See you soon")
 
 # 1. Check if user is new or old.
-    # if old then ask for password and check.
-    # if new then setup.
+# if old then ask for password and check.
+# if new then setup.
 
 # 2. Options to put new password, show passwords, or change master password.
 
