31b32cd22c3aa2f887f53b90d5b9652dc35f62a3
SoulNinja-dev <aadhi123awesome@gmail.com>
2021-01-08 14:32:59 +0530

    newUser() function completed and working

--- pass_mangr.py
+++ pass_mangr.py
@@ -1,5 +1,8 @@
 import mysql.connector
 import os
+import time
+
+# __init__
 db = mysql.connector.connect(
     host="localhost",
     user="soulninja",
@@ -7,59 +10,54 @@ db = mysql.connector.connect(
     database="password_manager"
 )
 
+# global variables
 cursor = db.cursor()
+choice = 0
 
-name = input("Enter your name: ")
-isNewUser=0
-isOldUser=0
-# methods 
-
-def signup():
-    print('Welcome, Hope you like your stay here at password manager,')
-    print('We keep all your passwords safe and secure! ')
-    print('Please enter your name again')
-    global name
-    input(name)
+# methods
 
 
-def login():
-    
-    qry="SELECT name FROM main WHERE name=%s"  # checking if user exists
-    cursor.execute(qry, (name,))
-
-    isUser=cursor.fetchone()
-    global isNewUser 
-    global isOldUser
-    if isUser == None:
-        isNewUser = 1
-        print('Hello there new user, Let me take you to sign up.././')
-    else:
-        isOldUser = 1
-        print('Hi there '+name+' Welcome back to password manager')
-# main() 
-login()
-
-if(isNewUser==1):
-    signup()
-else:
-    print('So what u wanna do today')
+def choice():
+    print("Hey, enter your choice: ")
+    print('1. New User')
+    print('2. Returning User')
+    print('3. Exit')
+    global choice
+    choice = input()
 
 
+def newUser():
+    print('Hey enter your name: ')
+    name = input()
+    print('got it! now enter the website you want to store your password for')
+    website = input()
+    print('got that, now your secret password: ')
+    password = input()
+    cursor.execute(
+        'INSERT INTO main (name,website,password) VALUES (%s,%s,%s)', (name, website, password))
+    print('Yep got it! You can check back any time to retrieve your password')
+    db.commit()
+
+# main()
+choice()
+print(choice)
+newUser()
+time.sleep(5)
+cursor.execute('SELECT * FROM main')
 
 
-
-# CREATE TABLE main(
-#    -> id INT AUTO_INCREMENT,
-#    -> name VARCHAR(100),
-#    -> website VARCHAR(255),
-#    -> password VARCHAR(255),
-#    -> PRIMARY KEY(id)
+# comments for later reference
 
 
-
-# 1. make sign up method which will allow the user to sign up.
-
-# 2. Options to put new password, show passwords, or change master password.
-
-# 3. Create a table for each user.
-
+# def login():
+# qry = "SELECT name FROM main WHERE name=%s"  # checking if user exists
+#cursor.execute(qry, (name,))
+#isUser = cursor.fetchone()
+# global isNewUser
+#  global isOldUser
+#   if isUser == None:
+#        isNewUser = 1
+#        print('Hello there new user, Let me take you to sign up.././')
+#    else:
+#        isOldUser = 1
+#        print('Hi there '+name+' Welcome back to password manager')
